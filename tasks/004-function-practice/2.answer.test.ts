fdbc2b0af854fe8388343f0be1a91a68dbf3bde4
Miguel Aragon <8313806+MiguelAraCo@users.noreply.github.com>
2021-11-17 15:14:51 +0900

    Partially add task 4

---
+++ tasks/004-function-practice/2.answer.test.ts
@@ -0,0 +1,28 @@
+// Write code here!
+
+// ============ DO NOT EDIT ============
+
+// === Tests
+describe("Function practice", () => {
+  describe("getAverage", () => {
+    test("is defined", () => expect(getAverage).toBeDefined());
+    test("is function", () => expect(typeof getAverage).toEqual("function"));
+  });
+  describe("getMaximum", () => {
+    test("is defined", () => expect(getMaximum).toBeDefined());
+    test("is function", () => expect(typeof getMaximum).toEqual("function"));
+  });
+  describe("getMinimum", () => {
+    test("is defined", () => expect(getMinimum).toBeDefined());
+    test("is function", () => expect(typeof getMinimum).toEqual("function"));
+  });
+  describe("getMedian", () => {
+    test("is defined", () => expect(getMedian).toBeDefined());
+    test("is function", () => expect(typeof getMedian).toEqual("function"));
+  });
+  describe("getStatistics", () => {
+    test("is defined", () => expect(getStatistics).toBeDefined());
+    test("is function", () => expect(typeof getStatistics).toEqual("function"));
+  });
+});
+// === End: Tests
